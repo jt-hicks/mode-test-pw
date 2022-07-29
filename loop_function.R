82418f3364980fafbec7198a2c048e45b7a7baa5
jthicks <jthicks89@gmail.com>
2022-07-29 10:29:04 +0100

    Parallelize the loop

--- loop_function.R
+++ loop_function.R
@@ -15,6 +15,9 @@ pf_loop <- function(data_file,n_loop,n_particles=100,volatility=0.5,freq=1){
            log = TRUE)
   }
   
+  lik_list <- function(i,p,pars){
+    p$run(pars)
+  }
   stochastic_schedule <- seq(from = 60, by = 30/freq, to = 1830)
   
   model <- odin.dust::odin_dust("toyodinmodel.R")
@@ -25,14 +28,17 @@ pf_loop <- function(data_file,n_loop,n_particles=100,volatility=0.5,freq=1){
                nrates = 15)
   n_particles <- c(10,50,100,200,500)
   lik_hist <- matrix(nrow=n_loop,ncol=length(n_particles))
+  #parallel::clusterExport(cl=NULL,varlist = c('mode_toyodinmodel_alloc'))
+  start.time <- Sys.time()
   for (k in 1:length(n_particles)){
-    for(i in 1:n_loop){
-      p <- mcstate::particle_filter$new(data, model, n_particles[k], compare,
-                                        index = index, seed = 2L,
-                                        stochastic_schedule = stochastic_schedule)
-      lik_hist[i,k] <- p$run(pars)
-    }
+    p <- mcstate::particle_filter$new(data, model, n_particles[k], compare,
+                                      index = index, seed = 1L,
+                                      stochastic_schedule = stochastic_schedule,
+                                      n_threads=4)
+    lik_hist[,k] <- t(unlist(lapply(1:n_loop,FUN = lik_list, p=p, pars=pars)))
+    # lik_hist[,k] <- t(unlist(lapply(1:n_loop,lik_list, p=p, pars=pars)))
   }
+  print(Sys.time()-start.time)
   df <- as.data.frame(lik_hist)
   colnames(df) <- n_particles
   return(df)
--- run_loop.R
+++ run_loop.R
@@ -1,22 +1,32 @@
 #Run loop function
-setwd('Q:/mode-test-main')
-
+setwd('Q:/mode-test-pw')
+drat:::add("mrc-ide") # install.packages("drat") if this errors, then try again
+install.packages("didehpc")
 library(didehpc)
 library(pkgdepends)
 
 root <- "contexts"
 sources <- "loop_function.R"
+config <- didehpc::didehpc_config(cores = 4, parallel = TRUE)
+
 
 ctx <- context::context_save("contexts", sources = sources,
+                             #packages = c("parallelly"),
                              package_sources = conan::conan_sources(c("mrc-ide/odin.dust",'mrc-ide/mcstate')))
-obj <- didehpc::queue_didehpc(ctx)
+
+obj <- didehpc::queue_didehpc(ctx,config = config)
 
 obj$cluster_load(TRUE)
+obj$config
 
-t <- obj$enqueue(pf_loop(data='casedata_monthly.csv',n_loop = 10))
+t <- obj$enqueue(pf_loop(data='casedata_monthly.csv',n_loop = 500))
+start.time <- Sys.time()
+pf_loop(data='casedata_monthly.csv',n_loop = 10)
+print(Sys.time()-start.time)
+
 t$status()
 t$result()
-t$wait(100)
+t$wait(500)
 
 t$times()
 t$log()
