6ce7299107be0eccb08ff2bea66faca44f4fcd84
jthicks <jthicks89@gmail.com>
2022-09-21 11:46:55 +0100

    Run and summarize pmcmc runs

--- pmcmc_cluster.R
+++ pmcmc_cluster.R
@@ -9,6 +9,10 @@ library('mcstate')
 library(didehpc)
 library(pkgdepends)
 library(dplyr)
+library("coda")
+library(binom)
+library(ggplot2)
+library(bayesplot)
 
 data_raw <- readRDS('data_raw_120822.RDS')
 root <- "contexts"
@@ -132,3 +136,184 @@ sim_test <- run_pmcmc(data=data_raw,EIR_vol=0.1,init_EIR = 20,n_particles = 100,
                      atol = 1e-4,
                      rtol = 1e-4)
 saveRDS(sim_test,'sim_test.RDS')
+proposal <- matrix(c(0.0336,-0.000589,-0.000589,0.04942),ncol=2)
+source("run_pmcmc.R")
+
+pmcmc_eq <- run_pmcmc(data=data_raw_cmis_all,
+                      # EIR_vol=0.3,
+                      # init_EIR = 5,
+                      n_particles = 200,
+                      max_steps = 1e6,
+                      atol = 1e-6,
+                      rtol = 1e-6,
+                      proposal_matrix = proposal,
+                      n_steps = 1000)
+saveRDS(pmcmc_eq,'cmis_all_1.RDS') #good run
+eq_mcmc <- coda::as.mcmc(cbind(pmcmc_eq$probabilities, pmcmc_eq$pars))
+1 - coda::rejectionRate(eq_mcmc)
+coda::effectiveSize(eq_mcmc)
+proposal <-  cov(pmcmc_eq$pars)
+summary(eq_mcmc)
+windows(60,50)
+plot(eq_mcmc)
+history2 <- pmcmc_eq$trajectories$state
+plot_particle_filter(history2,true_history=data_raw_cmis_all,times=data_raw_cmis_all$t)
+matplot(data_raw_cmis_all$t, t(history2[2, , -1]), type = "l",
+        xlab = "Time", ylab = "EIR",
+        col = "#A6CEE3", lty = 1, ylim = range(history2[2, , -1]))
+
+matplot(data_raw_cmis_all$t, t(history[2, , -1]), type = "l",
+        xlab = "Time", ylab = "EIR",
+        col = "#A6CEE3", lty = 1, ylim = range(c(0,100)))
+
+root <- "contexts"
+sources <- c("run_pmcmc.R",
+             "MiP-given/model_parameters.R","MiP-given/equilibrium-init-create-stripped.R")
+config <- didehpc::didehpc_config(cores = 4, parallel = TRUE)
+
+
+ctx <- context::context_save("contexts", sources = sources,
+                             package_sources = conan::conan_sources(c("mrc-ide/odin.dust",'mrc-ide/mcstate')))
+
+obj <- didehpc::queue_didehpc(ctx,config = config)
+
+obj$cluster_load(TRUE)
+obj$config
+obj$login()
+pmcmc_chain1 <- obj$enqueue(run_pmcmc(data=data_raw_cmis_all,
+                                    # EIR_vol=0.3,
+                                    # init_EIR = 5,
+                                    n_particles = 200,
+                                    max_steps = 1e6,
+                                    atol = 1e-6,
+                                    rtol = 1e-6,
+                                    proposal_matrix = proposal,
+                                    n_steps = 1000))
+pmcmc_chain1$id
+pmcmc_chain1$status()
+pmcmc_chain1$times()
+pmcmc_chain1$log()
+
+pmcmc_chain2 <- obj$enqueue(run_pmcmc(data=data_raw_cmis_all,
+                                      # EIR_vol=0.3,
+                                      # init_EIR = 5,
+                                      n_particles = 200,
+                                      max_steps = 1e6,
+                                      atol = 1e-6,
+                                      rtol = 1e-6,
+                                      proposal_matrix = proposal,
+                                      n_steps = 1000))
+pmcmc_chain2$id
+pmcmc_chain2$status()
+pmcmc_chain2$times()
+pmcmc_chain2$log()
+
+pmcmc_chain3 <- obj$enqueue(run_pmcmc(data=data_raw_cmis_all,
+                                      # EIR_vol=0.3,
+                                      # init_EIR = 5,
+                                      n_particles = 200,
+                                      max_steps = 1e6,
+                                      atol = 1e-6,
+                                      rtol = 1e-6,
+                                      proposal_matrix = proposal,
+                                      n_steps = 1000))
+pmcmc_chain3$id
+pmcmc_chain3$status()
+pmcmc_chain3$times()
+pmcmc_chain3$log()
+
+##Get results from run 1 and 3
+pmcmc_run1 <- pmcmc_chain1$result()
+pmcmc_run2 <- pmcmc_chain2$result()
+pmcmc_run3 <- pmcmc_chain3$result()
+
+run1_mcmc <- coda::as.mcmc(cbind(pmcmc_run1$probabilities, pmcmc_run1$pars))
+1 - coda::rejectionRate(run1_mcmc)
+coda::effectiveSize(run1_mcmc)
+proposal <-  cov(pmcmc_run1$pars)
+summary(run1_mcmc)
+windows(60,50)
+plot(run1_mcmc)
+history_run1 <- pmcmc_run1$trajectories$state
+plot_particle_filter(history_run1,true_history=data_raw_cmis_all,times=data_raw_cmis_all$t)
+matplot(data_raw_cmis_all$t, t(history_run1[2, , -1]), type = "l",
+        xlab = "Time", ylab = "EIR",
+        col = "#A6CEE3", lty = 1, ylim = range(history_run1[2, , -1]))
+saveRDS(pmcmc_run1,'cmis_all_run1.RDS') #good run
+
+run2_mcmc <- coda::as.mcmc(cbind(pmcmc_run2$probabilities, pmcmc_run2$pars))
+1 - coda::rejectionRate(run2_mcmc)
+coda::effectiveSize(run2_mcmc)
+proposal <-  cov(pmcmc_run2$pars)
+summary(run2_mcmc)
+windows(60,50)
+plot(run2_mcmc)
+history_run2 <- pmcmc_run2$trajectories$state
+plot_particle_filter(history_run2,true_history=data_raw_cmis_all,times=data_raw_cmis_all$t)
+matplot(data_raw_cmis_all$t, t(history_run2[2, , -1]), type = "l",
+        xlab = "Time", ylab = "EIR",
+        col = "#A6CEE3", lty = 1, ylim = range(history_run2[2, , -1]))
+saveRDS(pmcmc_run2,'cmis_all_run2.RDS') #good run
+
+
+run3_mcmc <- coda::as.mcmc(cbind(pmcmc_run3$probabilities, pmcmc_run3$pars))
+1 - coda::rejectionRate(run3_mcmc)
+coda::effectiveSize(run3_mcmc)
+proposal <-  cov(pmcmc_run3$pars)
+summary(run3_mcmc)
+windows(60,50)
+plot(run3_mcmc)
+history_run3 <- pmcmc_run3$trajectories$state
+plot_particle_filter(history_run3,true_history=data_raw_cmis_all,times=data_raw_cmis_all$t)
+matplot(data_raw_cmis_all$t, t(history_run3[2, , -1]), type = "l",
+        xlab = "Time", ylab = "EIR",
+        col = "#A6CEE3", lty = 1, ylim = range(history_run3[2, , -1]))
+saveRDS(pmcmc_run3,'cmis_all_run3.RDS') #good run
+
+all_runs <- as.mcmc(rbind(run1_mcmc[101:1000,],run2_mcmc[101:1000,],run3_mcmc[101:1000,]))
+1 - coda::rejectionRate(all_runs)
+coda::effectiveSize(all_runs)
+plot(all_runs)
+raft <- raftery.diag(run1_mcmc)
+all_chains <- mcmc.list(run1_mcmc,run2_mcmc,run3_mcmc)
+run1_mcmc_proc <- coda::as.mcmc(cbind(pmcmc_run1$probabilities[51:1000,], pmcmc_run1$pars[51:1000,]))
+run2_mcmc_proc <- coda::as.mcmc(cbind(pmcmc_run2$probabilities[51:1000,], pmcmc_run2$pars[51:1000,]))
+run3_mcmc_proc <- coda::as.mcmc(cbind(pmcmc_run3$probabilities[51:1000,], pmcmc_run3$pars[51:1000,]))
+all_chains_proc <- mcmc.list(run1_mcmc_proc,run2_mcmc_proc,run3_mcmc_proc)
+
+1 - coda::rejectionRate(all_chains_proc)
+coda::effectiveSize(all_chains_proc)
+plot(all_chains_proc)
+summary(all_chains)
+color_scheme_set("mix-pink-blue")
+windows(60,50)
+((bayesplot::mcmc_trace(all_chains_proc,pars = 'log_prior')+mcmc_dens_overlay(all_chains_proc,pars = 'log_prior'))/
+  (bayesplot::mcmc_trace(all_chains_proc,pars = 'log_likelihood')+mcmc_dens_overlay(all_chains_proc,pars = 'log_likelihood'))/
+  (bayesplot::mcmc_trace(all_chains_proc,pars = 'log_posterior')+mcmc_dens_overlay(all_chains_proc,pars = 'log_posterior'))/
+  (bayesplot::mcmc_trace(all_chains_proc,pars = 'EIR_SD')+mcmc_dens_overlay(all_chains_proc,pars = 'EIR_SD'))/
+  (bayesplot::mcmc_trace(all_chains_proc,pars = 'log_init_EIR')+mcmc_dens_overlay(all_chains_proc,pars = 'log_init_EIR'))) + plot_layout(guides = "collect") #& theme(legend.position = "bottom")
+plot_particle_filter(history_run3,true_history=data_raw_cmis_all,times=data_raw_cmis_all$t)
+
+
+ages_run1 <- history_run1[4:24,,-1]
+#[agegroup,mcmc sample,time]
+history_run1[4,101:1000,]
+df_ages <- data.frame(agegroup=numeric(),
+                      month=numeric(),
+                      value=numeric())
+for(age in 4:24){
+  for(time in 2:62){
+    temp <- data.frame(agegroup=age-3,
+                       month=time,
+                       value=history_run1[age,101:1000,time])
+    df_ages <- rbind(df_ages,temp)
+  }
+}
+windows(160,90)
+ggplot(df_ages,aes(x=as.factor(agegroup),y=value))+
+  geom_boxplot()+
+  facet_wrap(~month)
+
+ggplot(df_ages[df_ages$month<=13,],aes(x=as.factor(agegroup),y=value))+
+  geom_boxplot()+
+  facet_wrap(~month)
