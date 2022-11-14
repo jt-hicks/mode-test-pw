826f1f11b02f438539f9a3743ca58a2929ac5951
jthicks <jthicks89@gmail.com>
2022-11-14 09:30:49 +0000

    Separate file for function to add binomial confidence intervals

---
+++ addCIs.R
@@ -0,0 +1,10 @@
+addCIs<-function(df,Ys,Ns){
+  df$mean<-NA
+  df$upper<-NA
+  df$lower<-NA
+  CIs<-binom.confint(Ys,Ns,method="exact")
+  df$mean[Ns>0]<-CIs$mean[Ns>0]
+  df$upper[Ns>0]<-CIs$upper[Ns>0]
+  df$lower[Ns>0]<-CIs$lower[Ns>0]
+  return(df)
+}
